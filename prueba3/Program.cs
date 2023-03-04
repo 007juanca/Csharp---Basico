b5dd0b899c52f0db5cb2113835f3862acb8e8c78
007juanca <jgarciam114@miumg.edu.gt>
2023-03-03 23:27:06 -0600

    se agrego repositorio

---
+++ prueba3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace fundamentos
+{
+    internal class EntradaSalida
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.Write("Escribe tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+            Console.Read();
+        }
+    }
+}
